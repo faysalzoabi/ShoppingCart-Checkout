c9afa6dc1f36c607ef2dffa339ece4d2fe35d9ef
Faysal AlZoubi <faysal.zoabi@gmail.com>
2018-12-26 10:49:22 +0100

    Updates

---
+++ precache-manifest.9e29fc0c69772255a707995149b9ae98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45b4770452a32be91323",
+    "url": "/ShoppingCart-Checkout/static/js/runtime~main.45b47704.js"
+  },
+  {
+    "revision": "ddf31b47d1af8108ff0a",
+    "url": "/ShoppingCart-Checkout/static/js/main.ddf31b47.chunk.js"
+  },
+  {
+    "revision": "dc7321c6d1b8dc6f4c76",
+    "url": "/ShoppingCart-Checkout/static/js/1.dc7321c6.chunk.js"
+  },
+  {
+    "revision": "ddf31b47d1af8108ff0a",
+    "url": "/ShoppingCart-Checkout/static/css/main.def24fbf.chunk.css"
+  },
+  {
+    "revision": "f5e9e07b7739df0666b9ec1b42c302f8",
+    "url": "/ShoppingCart-Checkout/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ShoppingCart-Checkout/precache-manifest.9e29fc0c69772255a707995149b9ae98.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ShoppingCart-Checkout/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
